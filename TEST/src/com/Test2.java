bd2373d16c84f209e4a74cf13fad53e0eb3b7378
duytk <duyhyhy@gmail.com>
2014-10-21 16:59:02 +0700

    edit Test2.java

--- TEST/src/com/Test2.java
+++ TEST/src/com/Test2.java
@@ -2,6 +2,6 @@ package com;
 
 public class Test2 {
 	public static void main(String[] args){
-		System.out.println("Hell yeah! - edited");
+		System.out.println("Hell yeah! - edited - edited again");
 	}
 }
